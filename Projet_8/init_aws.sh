78d6fc97ff1e5d63bd1c258644e43b76de5d3f15
William David <william.david93000@gmail.com>
2023-12-19 01:23:55 +0100

    projet_8 18122023

---
+++ Projet_8/init_aws.sh
@@ -0,0 +1,17 @@
+sudo python3 -m pip install -U setuptools
+sudo python3 -m pip install -U pip
+sudo python3 -m pip install wheel
+sudo python3 -m pip install pillow
+sudo python3 -m pip install pandas==1.2.5
+sudo python3 -m pip install pyarrow
+sudo python3 -m pip install boto3
+sudo python3 -m pip install s3fs
+sudo python3 -m pip install fsspec
+sudo python3 -m pip install jupyterlab
+sudo python3 -m pip install jupyter
+sudo python3 -m pip install notebook
+sudo python3 -m pip install pyspark
+sudo python3 -m pip install tensorflow
+sudo python3 -m pip install seanborn
+sudo python3 -m pip install matplotlib
+sudo python3 -m pip install findspark
